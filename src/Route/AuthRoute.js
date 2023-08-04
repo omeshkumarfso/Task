5323d99c4baa6484fb019f2dcf8dcd185c5f0353
omeshkumarfso <omeshkumarfso@gmail.com>
2023-08-04 13:16:28 +0530

    first commit

---
+++ src/Route/AuthRoute.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AuthRoute = () => {
+  return (
+    <View>
+      <Text>AuthRoute</Text>
+    </View>
+  )
+}
+
+export default AuthRoute
